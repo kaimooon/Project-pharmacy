a0d6f96646b4c07118083cc90ff4608a8411b40e
kaimooon <118114390+kaimooon@users.noreply.github.com>
2023-11-28 20:29:21 +0800

    Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace midterm_project
 {
@@ -43,8 +42,9 @@ namespace midterm_project
             }
 
             else if (user_Input == "T")
-            {
+            {            
                 TransactionHistory history = new TransactionHistory();
+                history.ViewAllTransactions();
             }
 
             else if (user_Input == "E")
