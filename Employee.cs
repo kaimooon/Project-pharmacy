bd9a76a6ecfbd8fa56b7bfc7a0e22d866d97c212
kaimooon <118114390+kaimooon@users.noreply.github.com>
2023-11-30 23:16:23 +0800

    Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -19,7 +19,7 @@ namespace midterm_project
             Console.WriteLine("Press 'A' if you want to add item or stock in the inventory.");
             Console.WriteLine();
             Console.WriteLine("Press 'R' if you want to remove stock in the inventory.");
-            Console.WriteLine();      
+            Console.WriteLine();
             Console.WriteLine("Press 'T' if you want to view the transaction history.");
             Console.WriteLine();
             Console.WriteLine("Press 'S' if you want to search for an item in the inventory.");
@@ -32,23 +32,19 @@ namespace midterm_project
                 Inventory inventory = new Inventory();
                 inventory.view();
             }
-
             else if (user_Input == "A")
             {
                 add();
             }
-
             else if (user_Input == "R")
             {
                 remove();
             }
-
             else if (user_Input == "T")
-            {            
+            {
                 TransactionHistory history = new TransactionHistory();
                 history.ViewAllTransactions();
             }
-
             else if (user_Input == "S")
             {
                 Console.WriteLine("Type in the name of the product: ");
@@ -57,12 +53,10 @@ namespace midterm_project
                 SearchEngine searchEngine = new SearchEngine();
                 searchEngine.SearchProductInInventory(partialName);
             }
-
             else if (user_Input == "E")
             {
                 return;
             }
-
             else
             {
                 Console.WriteLine("Invalid Input. Please Input 'V', 'A', 'R', 'T', or 'E'.");
@@ -71,15 +65,7 @@ namespace midterm_project
 
         private int ParseIntValue(string input, string columnName)
         {
-            if (int.TryParse(input, out int result))
-            {
-                return result;
-            }
-            else
-            {
-                Console.WriteLine($"Failed to parse integer value from column '{columnName}' with input: {input}");
-                return 0; // Or any default value that makes sense in your context
-            }
+            return int.TryParse(input, out int result) ? result : 0;
         }
 
         private double ParseDoubleValue(string input, string columnName)
@@ -96,220 +82,152 @@ namespace midterm_project
 
             string numericPart = numericPartBuilder.ToString();
 
-            if (double.TryParse(numericPart, out double result))
-            {
-                return result;
-            }
-            else
-            {
-                Console.WriteLine($"Failed to parse double value from column '{columnName}' with input: {input}");
-                return 0.0; // Or any default value that makes sense in your context
-            }
+            return double.TryParse(numericPart, out double result) ? result : 0.0;
         }
 
         private void DisplayLowStockWarning()
         {
-            try
-            {
-                string filePath = "inventory.csv";
-                string[] lines = File.ReadAllLines(filePath);
+            string filePath = "inventory.csv";
+            string[] lines = File.ReadAllLines(filePath);
 
-                foreach (string line in lines)
+            foreach (string line in lines)
+            {
+                string[] fields = line.Split(',');
+
+                if (int.TryParse(fields[3], out int quantity) && quantity <= 5)
                 {
-                    string[] fields = line.Split(',');
-
-                    if (int.TryParse(fields[3], out int quantity) && quantity <= 5)
-                    {
-                        string itemName = fields[1];
-                        Console.WriteLine($"WARNING: LOW STOCK FOR {itemName.ToUpper()}!");
-                    }
+                    string itemName = fields[1];
+                    Console.WriteLine($"WARNING: LOW STOCK FOR {itemName.ToUpper()}!");
                 }
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine($"An error occurred while reading the file: {e.Message}");
             }
         }
 
         private void add()
         {
-            try
+            string mainInventoryPath = "inventory.csv";
+            string newStockPath = "newstock.csv";
+
+            string[] mainInventoryLines = File.ReadAllLines(mainInventoryPath);
+            string[] newStockLines = File.ReadAllLines(newStockPath).Skip(1).ToArray(); // Skip the header line
+
+            List<string> updatedMainInventory = new List<string>(mainInventoryLines);
+
+            int lastProductId = 0;
+
+            foreach (string line in updatedMainInventory)
             {
-                string mainInventoryPath = "inventory.csv";
-                string newStockPath = "newstock.csv";
-
-                string[] mainInventoryLines = File.ReadAllLines(mainInventoryPath); // Read all lines, including the header line
-                string[] newStockLines = File.ReadAllLines(newStockPath).Skip(1).ToArray(); // Skip the header line
-
-                List<string> updatedMainInventory = new List<string>(mainInventoryLines);
-
-                int lastProductId = updatedMainInventory
-                    .Skip(1) // Skip the header line
-                    .Select(line => int.Parse(line.Split(',')[0]))
-                    .DefaultIfEmpty(0)
-                    .Max(); // Get the maximum product ID
-
-                foreach (string newProduct in newStockLines)
+                string[] columns = line.Split(',');
+                if (columns.Length > 0 && int.TryParse(columns[0], out int currentId) && currentId > lastProductId)
                 {
-                    string[] newProductColumns = newProduct.Split(',');
+                    lastProductId = currentId;
+                }
+            }
 
-                    if (newProductColumns.Length < 3)
+            foreach (string newProduct in newStockLines)
+            {
+                string[] newProductColumns = newProduct.Split(',');
+
+                if (newProductColumns.Length < 3)
+                {
+                    Console.WriteLine($"Invalid format in the new stock entry: {newProduct}");
+                    continue; // Skip the current iteration and move to the next one
+                }
+
+                string productName = newProductColumns[0];
+                string priceString = newProductColumns[1].Trim(); // Trim spaces
+
+                if (int.TryParse(priceString.Substring(1), out int price) && int.TryParse(newProductColumns[2], out int stockQuantity))
+                {
+                    bool productFound = false;
+
+                    for (int i = 0; i < updatedMainInventory.Count; i++)
                     {
-                        Console.WriteLine($"Invalid format in the new stock entry: {newProduct}");
-                        continue; // Skip the current iteration and move to the next one
-                    }
+                        string[] columns = updatedMainInventory[i].Split(',');
 
-                    string productName = newProductColumns[0];
-                    string priceString = newProductColumns[1].Trim(); // Trim spaces
-
-                    if (int.TryParse(priceString.Substring(1), out int price) && int.TryParse(newProductColumns[2], out int stockQuantity))
-                    {
-                        bool productFound = false;
-
-                        for (int i = 0; i < updatedMainInventory.Count; i++)
+                        if (columns.Length >= 4 && columns[1].Trim() == productName)
                         {
-                            string[] columns = updatedMainInventory[i].Split(',');
-
-                            if (columns.Length >= 4 && columns[1].Trim() == productName)
+                            int currentStock;
+                            if (int.TryParse(columns[3], out currentStock))
                             {
-                                // Update the existing product's stock quantity
-                                int currentStock;
-                                if (int.TryParse(columns[3], out currentStock))
-                                {
-                                    currentStock += stockQuantity;
-                                    columns[3] = currentStock.ToString();
-                                    updatedMainInventory[i] = string.Join(",", columns);
-                                    productFound = true;
-                                    break;
-                                }
+                                currentStock += stockQuantity;
+                                columns[3] = currentStock.ToString();
+                                updatedMainInventory[i] = string.Join(",", columns);
+                                productFound = true;
+                                break;
                             }
                         }
-
-                        if (!productFound)
-                        {
-                            // Increment the last product ID and use it for the new product
-                            lastProductId++;
-                            string formattedPrice = $"P{price.ToString()}"; // Assuming price format: P + numeric value
-                            string newProductLine = $"{lastProductId},{productName},{formattedPrice},{stockQuantity}";
-                            updatedMainInventory.Add(newProductLine);
-                        }
                     }
-                    else
+
+                    if (!productFound)
                     {
-                        Console.WriteLine($"Invalid format in the new stock entry: {newProduct}");
-                        continue; // Skip the current iteration and move to the next one
-                    }
-                }
-
-                // Write the updated content back to the file
-                File.WriteAllLines(mainInventoryPath, updatedMainInventory);
-
-                Console.WriteLine("Stock updated successfully!");
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine($"An error occurred while reading/writing the file: {e.Message}");
-            }
-        }
-
-        private int GenerateProductId(IEnumerable<string> lines)
-        {
-            // Replace this with your logic to generate a new product ID
-            // For simplicity, let's assume the product ID is one more than the maximum existing ID
-            int maxId = lines.Select(line => int.Parse(line.Split(',')[0])).DefaultIfEmpty(0).Max();
-            return maxId + 1;
-        }
-        private double ExtractNumericValue(string input)
-        {
-            StringBuilder numericPartBuilder = new StringBuilder();
-
-            foreach (char character in input)
-            {
-                if (char.IsDigit(character) || character == '.' || character == '-')
-                {
-                    numericPartBuilder.Append(character);
-                }
-            }
-
-            string numericPart = numericPartBuilder.ToString();
-
-            // Attempt to parse the numeric part to double
-            if (double.TryParse(numericPart, out double result))
-            {
-                return result;
-            }
-            else
-            {
-                // Handle the case where parsing fails
-                Console.WriteLine($"Failed to parse numeric value from input: {input}");
-                return 0.0; // Or any default value that makes sense in your context
-            }
-        }
-
-        private string GenerateProductId(string[] inventoryLines)
-        {
-            // You can implement your own logic to generate a unique product ID
-            // For simplicity, this example assumes that product IDs are numeric and increments from the last product ID in the inventory
-
-            if (inventoryLines.Length == 0)
-            {
-                return "1"; // Start with product ID 1 if the inventory is empty
-            }
-
-            string lastProductId = inventoryLines.Last().Split(',')[0];
-            if (int.TryParse(lastProductId, out int lastId))
-            {
-                return (lastId + 1).ToString();
-            }
-
-            // If parsing fails, return a default value
-            return "1";
-        }
-
-        private void remove()
-        {
-            try
-            {
-                string mainInventoryPath = "inventory.csv";
-
-                string[] mainInventoryLines = File.ReadAllLines(mainInventoryPath).Skip(1).ToArray(); // Skip the header line
-
-                Console.Write("Enter the Product ID to remove: ");
-                if (int.TryParse(Console.ReadLine(), out int productIdToRemove))
-                {
-                    bool productExists = mainInventoryLines.Any(line => int.Parse(line.Split(',')[0]) == productIdToRemove);
-
-                    if (productExists)
-                    {
-                        Console.WriteLine($"Are you sure you want to delete product with ID {productIdToRemove} from the inventory? (Y/N)");
-
-                        if (Console.ReadLine()?.ToUpper() == "Y")
-                        {
-                            mainInventoryLines = mainInventoryLines.Where(line => int.Parse(line.Split(',')[0]) != productIdToRemove).ToArray();
-
-                            // Write the updated content back to the file
-                            File.WriteAllLines(mainInventoryPath, mainInventoryLines);
-
-                            Console.WriteLine($"Product with ID {productIdToRemove} removed successfully!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Deletion canceled.");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Product with ID {productIdToRemove} not found in the inventory.");
+                        lastProductId++;
+                        string formattedPrice = $"P{price.ToString()}";
+                        string newProductLine = $"{lastProductId},{productName},{formattedPrice},{stockQuantity}";
+                        updatedMainInventory.Add(newProductLine);
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Invalid input. Please enter a valid Product ID.");
+                    Console.WriteLine($"Invalid format in the new stock entry: {newProduct}");
+                    continue; // Skips the current iteration and move to the next one
                 }
             }
-            catch (IOException e)
+
+            File.WriteAllLines(mainInventoryPath, updatedMainInventory);
+
+            Console.WriteLine("Stock updated successfully!");
+        }
+
+        private void remove()
+        {
+            string mainInventoryPath = "inventory.csv";
+
+            string[] mainInventoryLines = File.ReadAllLines(mainInventoryPath).Skip(1).ToArray(); // Skips the header line
+
+            Console.Write("Enter the Product ID to remove: ");
+            if (int.TryParse(Console.ReadLine(), out int productIdToRemove))
             {
-                Console.WriteLine($"An error occurred while reading/writing the file: {e.Message}");
+                bool productExists = false;
+
+                for (int i = 0; i < mainInventoryLines.Length; i++)
+                {
+                    string[] columns = mainInventoryLines[i].Split(',');
+
+                    if (columns.Length > 0)
+                    {
+                        int currentId;
+                        if (int.TryParse(columns[0], out currentId) && currentId == productIdToRemove)
+                        {
+                            productExists = true;
+
+                            Console.WriteLine($"Are you sure you want to delete product with ID {productIdToRemove} from the inventory? (Y/N)");
+                            if (Console.ReadLine()?.ToUpper() == "Y")
+                            {
+                                List<string> updatedInventory = new List<string>(mainInventoryLines);
+                                updatedInventory.RemoveAt(i);
+
+                                File.WriteAllLines(mainInventoryPath, updatedInventory);
+
+                                Console.WriteLine($"Product with ID {productIdToRemove} removed successfully!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Deletion canceled.");
+                            }
+
+                            break;
+                        }
+                    }
+                }
+
+                if (!productExists)
+                {
+                    Console.WriteLine($"Product with ID {productIdToRemove} not found in the inventory.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid Product ID.");
             }
         }
     }
